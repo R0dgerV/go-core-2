2bd16de95dbd3946bb09af7519aa0e96bd80b943
Andrey Bunyaev <a.bunyaev@formattwo.ru>
2021-04-21 09:57:14 +0300

    Добавление проверки + вывод ошибки

--- 01-fib/cmd/fibonache/main.go
+++ 01-fib/cmd/fibonache/main.go
@@ -4,9 +4,14 @@ import "fmt"
 import "go-core-2/01-fib/pkg/fibonache"
 
 func main() {
-	numbers := []int{1, 7, 10, 16, 20}
+	numbers := []int{1, 7, 10, 16, 23, 20, 45}
+	max := 20
 
 	for _, num := range numbers {
-		fmt.Println(fibonache.Number(num))
+		if num > max {
+			fmt.Println(fmt.Sprintf("Ошибка!!! Максимальное допустимое число: %d, Ваше число: %d", max, num))
+		}
+
+		fmt.Println(fmt.Sprintf("Для числа %d, фибоначе = %d", num, fibonache.Number(num)))
 	}
 }
